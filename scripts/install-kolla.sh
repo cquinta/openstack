90e3dc458efae1840863da7cf06453a95a15f400
Carlos Quintanilha <cquinta@gmail.com>
2022-10-14 18:10:42 -0300

    Add files via upload

---
+++ scripts/install-kolla.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+sudo pvcreate /dev/sdb
+sudo vgcreate cinder-volumes /dev/sdb
+sudo apt update -y
+sudo apt-get install net-tools -y
+sudo apt install python3-dev libffi-dev gcc libssl-dev -y
+sudo apt install python3-pip -y
+sudo pip3 install -U pip
+#sudo apt install ansible -y
+sudo pip3 install -U 'ansible>=4,<6'
+#sudo cp /vagrant/scripts/hosts /etc/hosts
+sudo pip3 install git+https://opendev.org/openstack/kolla-ansible@master
+sudo mkdir -p /etc/kolla
+sudo chown $USER:$USER /etc/kolla
+sudo cp -r /usr/local/share/kolla-ansible/etc_examples/kolla/* /etc/kolla
+sudo cp /usr/local/share/kolla-ansible/ansible/inventory/* .
+git clone --branch master https://opendev.org/openstack/kolla-ansible
+sudo pip3 install ./kolla-ansible
+sudo mkdir -p /etc/kolla
+sudo chown $USER:$USER /etc/kolla
+sudo cp -r kolla-ansible/etc/kolla/* /etc/kolla
+sudo cp kolla-ansible/ansible/inventory/* .
+kolla-ansible install-deps
+sudo mkdir /etc/ansible
+sudo cp ./scripts/ansible.cfg /etc/ansible/ansible.cfg
+kolla-genpwd
+sudo cp ./scripts/globals.yml /etc/kolla/globals.yml
+sudo cp ./scripts/hosts /etc/hosts
+kolla-ansible -i all-in-one bootstrap-servers
+kolla-ansible -i all-in-one prechecks
+kolla-ansible -i all-in-one deploy
